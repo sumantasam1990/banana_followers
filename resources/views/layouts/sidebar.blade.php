180ec0c86b196fd2ead5e897fc7ae8c4fc1d5efd
Sumanta Kundu <sumantasam1990@gmail.com>
2022-11-22 20:58:08 +0530

    from pc

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,35 @@
+<div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+    <div class="sidebar-brand d-none d-md-flex">
+        <h1 class="fs-4">Bananafollowers</h1>
+        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
+            <use xlink:href="assets/brand/coreui.svg#signet"></use>
+        </svg>
+    </div>
+    <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+        <li class="nav-item"><a class="nav-link" href="index.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
+                </svg> Dashboard</a></li>
+
+        <li class="nav-item"><a class="nav-link" href="colors.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-dollar"></use>
+                </svg> Add funds <span class="badge badge-sm bg-info ms-auto">$0.00</span></a></li>
+        <li class="nav-item"><a class="nav-link" href="typography.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-plus"></use>
+                </svg> New order</a></li>
+        <li class="nav-item"><a class="nav-link" href="typography.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-basket"></use>
+                </svg> Your orders</a></li>
+        <li class="nav-item"><a class="nav-link" href="typography.html">
+                <svg class="nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-gift"></use>
+                </svg> Gifts</a></li>
+
+        </li>
+
+    </ul>
+    <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
+</div>
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -1,33 +1,31 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@include('layouts.header')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    @vite(['resources/js/app.js'])
-</head>
-<body>
-<div class="container-fluid top-bar">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h1 class="fs-3 fw-bold text-uppercase">Bananafollowers.com</h1>
+<div class="wrapper d-flex flex-column min-vh-100 bg-light">
+    @include('layouts.top')
+    <div class="body flex-grow-1 px-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h2 class="fs-2 fw-bold">Add Fund</h2>
+                    <hr />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5">
+                    <form action="" method="post">
+                        @csrf
+                        <label class="form-label">Fund*</label>
+                        <input type="number" required name="_fund" class="form-control" placeholder="eg. $100" min="10">
+                        <div class="mt-3 d-grid gap-2 col-5">
+                            <button type="submit" class="btn btn-primary btn-lg">Pay now</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<div class="container mt-3">
-    <div class="row">
-        <div class="col-md-3">
-            <a href="" class="btn btn-warning">Button</a>
-        </div>
-        <div class="col-md-9"></div>
-    </div>
-</div>
-</body>
-</html>
+
+@include('layouts.footer')
+
